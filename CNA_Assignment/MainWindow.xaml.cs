97f65b81d1e7b493697c54ebde8e8bee47e4f94b
Jmakharashvili <m017735i@student.staffs.ac.uk>
2021-11-09 22:42:05 +0000

    Updated button functionality
    Finally added the send button which sends the message from one box to another. It also requires user to have a username inputed which will be displayed along with the message.

--- CNA_Assignment/MainWindow.xaml.cs
+++ CNA_Assignment/MainWindow.xaml.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Threading;
+using System.Windows.Threading;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
@@ -15,14 +17,60 @@ using System.Windows.Shapes;
 
 namespace CNA_Assignment
 {
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    /// 
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+            SendButton.Click += SendMessageDispacher;
+        }
+
+        private void SendMessageButton_Click(object sender, RoutedEventArgs e)
+        { 
+            if (messageInputBox.Text == "")
+            {
+                MessageBox.Show("No message in text box!", "Warning");
+            }
+            else
+            { 
+                string message = messageInputBox.Text;
+                messageInputBox.Text = "";
+                if (usernameBlock.Text == "Template")
+                { 
+                    MessageBox.Show("Please input your username!", "Warning");
+                    messageInputBox.Text = message;
+                }
+                else
+                { 
+                    string name = usernameBlock.Text;    
+                    MessageDisplay.Text += name + ": " + message + "\n";
+                }
+            }
+
+        }
+
+        private void SendMessageDispacher(object sender, RoutedEventArgs e)
+        {
+            string message = messageInputBox.Text;
+            
+            // The work to perform on another thread
+            ThreadStart ButtonThread = delegate()
+            { 
+                Dispatcher.Invoke(DispatcherPriority.Normal, new Action<string>(SendMessage), message);
+            };
+
+            // Creathe the thread and kick it started!
+            new Thread(ButtonThread).Start();
+        }
+
+        private void SendMessage(string status)
+        {
+            MessageDisplay.Text += usernameBlock.Text + ": " + status + "\n";
         }
     }
 }
--- CNA_Assignment/obj/Debug/net5.0-windows/App.g.cs
+++ CNA_Assignment/obj/Debug/net5.0-windows/App.g.cs
@@ -45,7 +45,7 @@ namespace CNA_Assignment {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.6.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.11.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace CNA_Assignment {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.6.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.11.0")]
         public static void Main() {
             CNA_Assignment.App app = new CNA_Assignment.App();
             app.InitializeComponent();
